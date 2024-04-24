6e97a1f6457e5b07d3e5b397254b9d99c3076a0c
shapast <aleka002@osloskolen.no>
2024-04-24 10:59:04 +0200

    Update section1.svelte

--- src/components/section1.svelte
+++ src/components/section1.svelte
@@ -3,15 +3,27 @@
 </script>
 
 <section>
-    <h1>sect 1</h1>
+
+    <button>START</button>
     <img src="https://quiet-com.github.io/quiet-assets/bølger.png" alt="Bølger">
+    
 </section>
 
 <style>
     img {
         width: 100vw;
     }
+    button {
+        z-index: 1;
+        margin:0;
+        position: relative;
+        top: 50%;
+
+    }
     section {
+        justify-content: center;
+        align-items: center;
         background-color: white !important;
     }
+
 </style>
