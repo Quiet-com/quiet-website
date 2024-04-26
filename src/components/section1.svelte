e0fabce2b732be0a8444b2ec3560494c9b7039cc
CUPZYY <richbanana1234@gmail.com>
2024-04-26 12:38:41 +0200

    button css

--- src/components/section1.svelte
+++ src/components/section1.svelte
@@ -18,7 +18,12 @@
         margin:0;
         position: relative;
         top: 50%;
-
+        padding: 0em 0.4em 0.2em 0.4em;
+        font-size: 5em;
+        border: white solid;
+        border-radius: 20px;
+        background-color: transparent;
+        font-family: Integral;
     }
     section {
         justify-content: center;
