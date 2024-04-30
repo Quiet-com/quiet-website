d7096212dda92df18f40789d8bebbb5259356533
CUPZYY <richbanana1234@gmail.com>
2024-04-30 22:36:14 +0200

    Added 3d model and sampletext on section 2

--- src/components/section2.svelte
+++ src/components/section2.svelte
@@ -1,15 +1,31 @@
 <script>
-    export let demoSection
+    export let demoSection;
+    import '@google/model-viewer';
+    import getAsset from '../util/assets';
 </script>
 
 <section bind:this={demoSection}>
-    <h2>
-        sect2
-    </h2>
+    <model-viewer disable-tap auto-rotate shadow-intensity="1" camera-controls touch-action="pan-y" src="{getAsset("3D/controller.glb")}"></model-viewer>
+    <div class="textbox">
+        <h1>Meet Quiet.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non luctus arcu. Maecenas quam tellus, bibendum eget magna at, eleifend vulputate erat. Cras pharetra volutpat odio et mattis. Pellentesque diam neque, mollis vitae neque vel, sagittis cursus sapien. Integer rhoncus, tellus vel sodales scelerisque, erat diam dignissim sem, at condimentum ex velit quis turpis. Donec a consequat lectus, eget laoreet nibh. Praesent pretium nunc odio. Nulla facilisi. Nulla tellus erat, bibendum bibendum molestie eu, consequat sit amet augue.</p>
+    </div>
+
 </section>
 
 <style>
     section {
         z-index: 1;
+        flex-direction: column;
+    }
+    .textbox {
+        width: 50%;
+    }
+    h1 {
+        font-size: 3rem;
+    }
+    model-viewer {
+        height: 100%;
+        width: 50%;
     }
 </style>
