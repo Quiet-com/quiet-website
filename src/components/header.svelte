a80c9a56f90bee41807deb8ddbad68319baf97c6
shapast <157977372+shapast@users.noreply.github.com>
2024-05-01 13:38:03 +0200

    Update header.svelte

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -2,7 +2,10 @@
 <main>
     <img id="logo" src="https://quiet-com.github.io/quiet-assets/SVG/QuietLogoIconWhite.svg" width=40vw alt="Loggos" on:click={reload}>
     <h1 on:click={reload}>Quiet.</h1>
-    <img id = "hamburger" src = "https://quiet-com.github.io/quiet-assets/Hamburger.png" alt = "Namnam Hamburger">
+    <div>
+        <img id = "hamburger" src = "https://quiet-com.github.io/quiet-assets/Hamburger.png" alt = "Namnam Hamburger">
+    </div>
+    
 </main>
 
 <script>
@@ -22,7 +25,6 @@
     h1 {
         align-self:center;
         display: flex;
-        justify-self: center;
         font-size: 27pt;
         color: #F1F2F3;
         font-family: Integral;
@@ -40,10 +42,15 @@
         align-self: center;
     }
 
+    div {
+        display: grid;
+        width: 100vw;
+    }
     #hamburger {
         display: flex;
         align-self: center;
         position: relative;
-        left: 82.5vw;
-    }
+       justify-self: flex-end;
+       right: 30px;
+}
 </style>
