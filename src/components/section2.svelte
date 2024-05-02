e8c1c8b35e9ced29d08230f0b517d2f658b1602b
CUPZYY <richbanana1234@gmail.com>
2024-05-02 20:58:18 +0200

    La til "BUY NOW" knapp

--- src/components/section2.svelte
+++ src/components/section2.svelte
@@ -8,7 +8,10 @@
     <model-viewer disable-tap camera-orbit="-155deg 55deg" auto-rotate shadow-intensity="1" camera-controls touch-action="pan-y" src="{getAsset("3D/controller.glb")}"></model-viewer>
     <div class="textbox">
         <h1>Meet Quiet.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non luctus arcu. Maecenas quam tellus, bibendum eget magna at, eleifend vulputate erat. Cras pharetra volutpat odio et mattis. Pellentesque diam neque, mollis vitae neque vel, sagittis cursus sapien. Integer rhoncus, tellus vel sodales scelerisque, erat diam dignissim sem, at condimentum ex velit quis turpis. Donec a consequat lectus, eget laoreet nibh. Praesent pretium nunc odio. Nulla facilisi. Nulla tellus erat, bibendum bibendum molestie eu, consequat sit amet augue.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non luctus arcu. Maecenas quam tellus, bibendum eget magna at, eleifend vulputate erat. Cras pharetra volutpat odio et mattis. Pellentesque diam neque, mollis vitae neque vel, sagittis cursus sapien.</p>
+        <button class="button">
+            BUY NOW
+        </button>
     </div>
 
 </section>
@@ -30,4 +33,23 @@
         height: 100%;
         width: 150%;
     }
+    .button {
+        width: 40%;
+        font-size: 1.3em;
+        font-weight: 600;
+        cursor: pointer;
+        height: 2.2em;
+        
+        border: none;
+        border-radius: 1.1em;
+        transition: all .4s ease-in-out;
+        background-size: 300% 100%;
+        background-image: linear-gradient(to right, #25aae1, #40e495, #30dd8a, #2bb673);
+        box-shadow: 0 4px 15px 0 rgba(49, 196, 190, 0.75);
+    }
+    .button:hover {
+        background-position: 100% 0;
+        transition: all .4s ease-in-out;
+    }
+
 </style>
