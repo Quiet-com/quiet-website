582986ae358e19132b5ef205af7e2ad2da966b79
CUPZYY <richbanana1234@gmail.com>
2024-05-21 23:47:32 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { vite as vidstack } from 'vidstack/plugins';
-import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  plugins: [vidstack()],
+  plugins: [svelte(), vidstack()],
 })
